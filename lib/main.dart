703964dfeb33756e7e8609d88a881d8a343d4653
brunomotadev <brunomotadev@gmail.com>
2021-06-05 13:40:07 -0300

    adicionado input/output dos dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:projeto_pessoa_imc/src/pessoa.dart';
+import 'dart:io';
 
 Pessoa pessoa = Pessoa();
 void main(List<String> arguments) {
-  
+  print('--- Escreva seu nome');
+  pessoa.nome = stdin.readLineSync().toString();
+  print('--- Escreva sua idade');
+  pessoa.idade = int.parse(stdin.readLineSync().toString());
+  print('--- Escreva seu peso');
+  pessoa.peso = double.parse(stdin.readLineSync().toString());
+  print('--- Escreva sua altura');
+  pessoa.altura = double.parse(stdin.readLineSync().toString());
 }
