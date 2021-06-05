6687883b275f8e27542eaeb8cc464e59869af05f
brunomotadev <brunomotadev@gmail.com>
2021-06-05 13:31:14 -0300

    exercicio final - projeto pessoa imc

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:projeto_pessoa_imc/src/pessoa.dart';
+
+Pessoa pessoa = Pessoa();
+void main(List<String> arguments) {
+  
+}
---
+++ lib/src/pessoa.dart
@@ -0,0 +1,6 @@
+class Pessoa {
+  late String nome;
+  late int idade;
+  late double altura;
+  late double peso;
+}
